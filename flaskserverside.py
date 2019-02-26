bf714c644ba05a61a7f0f71a113c72b39bfaf591
Jake <jake.townsend@pulteney.sa.edu.au>
2019-02-26 15:52:37 +1030

    Added a html page -J

--- flaskserverside.py
+++ flaskserverside.py
@@ -1,8 +1,20 @@
 from flask import Flask, jsonify, request, render_template, session, redirect
 import time
+import random
 app = Flask(__name__)
+curent_sound = 0
+@app.route('/')
+def page1():
+    return render_template("page1.html")
+
+@app.route('/update')
+def noot():
+    global curent_sound
+    return str(curent_sound)
+
 @app.route('/add', methods=['POST'])
 def add_value():
+    global curent_sound
     with open("sounddatatest.txt", "a+") as textfile:
         values = request.get_json()
         required = ['value']
@@ -10,6 +22,7 @@ def add_value():
             return 'Missing values', 400
         for line in values['value']:
             textfile.write("{}\t{} \r\n".format(line[0], line[1]))
+            curent_sound = line[0]
         print(values['value'])
         print(values['value'][0][0])
         return 'Done!'
