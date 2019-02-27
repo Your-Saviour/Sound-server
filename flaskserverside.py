c842b72aad93d311f832945a57ef0da02b4887a7
Jake <jake.townsend@pulteney.sa.edu.au>
2019-02-27 12:32:36 +1030

    Finished webserver -j

--- flaskserverside.py
+++ flaskserverside.py
@@ -1,8 +1,10 @@
 from flask import Flask, jsonify, request, render_template, session, redirect
 import time
 import random
+import glob
 app = Flask(__name__)
-curent_sound = 0
+curent_sound = {1: 0, 2: 0}
+locations = {1: "basment", 2: "kictchen"}
 @app.route('/')
 def page1():
     return render_template("page1.html")
@@ -10,22 +12,37 @@ def page1():
 @app.route('/update')
 def noot():
     global curent_sound
-    return str(curent_sound)
+    global locations
+    return "At {} it is {}. <br> At {} it is {}".format(locations[1], curent_sound[1], locations[2], curent_sound[2])
+
+@app.route('/admin')
+def controls():
+    listoffiles = list(glob.glob('static/*.txt'))
+    return render_template("guiint.html", listoffiles=listoffiles)
+
+@app.route('/change', methods=['POST'])
+def changer():
+    if "Location_1" in request.form:
+        locations[1] = request.form.get("Location_1")
+    if "Location_2" in request.form:
+        locations[2] = request.form.get("Location_2")
+    return redirect('/admin')
 
 @app.route('/add', methods=['POST'])
 def add_value():
     global curent_sound
-    with open("sounddatatest.txt", "a+") as textfile:
-        values = request.get_json()
-        required = ['value']
-        if not all(k in values for k in required):
-            return 'Missing values', 400
-        for line in values['value']:
-            textfile.write("{}\t{} \r\n".format(line[0], line[1]))
-            curent_sound = line[0]
-        print(values['value'])
-        print(values['value'][0][0])
+    global locations
+    values = request.get_json()
+    required = ['time', 'db','sensornum']
+    if not all(k in values for k in required):
+        return 'Missing values', 400
+    sensornum = values["sensornum"]
+    location = locations[sensornum]
+    with open("static/{}.txt".format(location), "a+") as textfile:
+        textfile.write("{}\t{}\t \r\n".format(values['db'], values['time'], values['sensornum']))
+        curent_sound[sensornum] = values['db']
         return 'Done!'
+
 
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0', port=80)
