72d1ebdc173451db4ba69f1bc63e3871c1b35a28
Jake <jake.townsend@pulteney.sa.edu.au>
2019-02-28 11:49:09 +1030

    ip test -j

---
+++ decibelsqrttest.py
@@ -0,0 +1,64 @@
+import math
+import time
+from datetime import datetime
+import requests
+
+def calculate_rms(sound_levels):
+    squared = []
+    for i in sound_levels:
+        squared.append(i**2)
+    
+    new = sum(squared)
+    e = math.sqrt(new)
+    return(e) 
+
+def scale(x1, y1, x2, y2, x):
+    numerator = y2*(x1 - x) + y1*(x - x2)
+    denominator = x1 - x2
+    
+    number = numerator/denominator
+    
+    return number
+
+
+import subprocess
+
+import serial
+ser = serial.Serial('/dev/cu.wchusbserial1410', 115200)  # open serial port
+print(ser.name)         # check which port was really used
+
+i = 0
+
+sound_levels = []
+
+while True:
+    
+    value_as_string = ser.readline().decode('utf-8').strip()
+    value_as_int = int(value_as_string)
+    
+    i = i + 1
+    
+    sound_levels.append(value_as_int)
+    
+    
+    if i == 100:
+        rms = calculate_rms(sound_levels) #* 0.275
+        log_rms = math.log(rms)
+        print(log_rms)
+        
+        decibels = scale(3.6, 40, 5.6, 80, log_rms)
+        #print(datetime.now(), decibels, ' db')    
+        
+        
+        date = datetime.now()
+        
+        data_to_send = {'time' : date.isoformat(), 'db' : decibels, 'sensornum': 1}
+        #print(data_to_send)
+        resp = requests.post('http://localhost:10001/add', json=data_to_send)
+        #print(resp.text)  
+        i = 0
+        sound_levels = []
+        
+ser.close()  
+
+
